0002b275f6cdd5c5da744cc82010817928da1bd3
YaoShangTseng <yakushou730@gmail.com>
2019-07-05 17:14:44 +0800

    Bridge Design Pattern

---
+++ structural/bridge/traditional/bridge_test.go
@@ -0,0 +1,106 @@
+package structural
+
+import (
+	"errors"
+	"strings"
+	"testing"
+)
+
+func TestPrinterAPI1(t *testing.T) {
+	api1 := PrinterImpl1{}
+	err := api1.PrintMessage("Hello")
+	if err != nil {
+		t.Errorf("Error trying to use the API1 implementation: Message: %s\n", err.Error())
+	}
+}
+
+type TestWriter struct {
+	Msg string
+}
+
+func (t *TestWriter) Write(p []byte) (n int, err error) {
+	n = len(p)
+	if n > 0 {
+		t.Msg = string(p)
+		return n, nil
+	}
+	err = errors.New("Content received on Writer was empty")
+	return
+}
+
+func TestPrinterAPI2(t *testing.T) {
+	api2 := PrinterImpl2{}
+	err := api2.PrintMessage("Hello")
+	if err != nil {
+		expectedErrorMessage := "You need to pass an io.Writer to PrinterImpl2"
+		if !strings.Contains(err.Error(), expectedErrorMessage) {
+			t.Errorf("Error message was not correct.\n Actual: %s\n Expected: %s\n", err.Error(), expectedErrorMessage)
+		}
+	}
+	TestWriter := TestWriter{}
+	api2 = PrinterImpl2{
+		Writer: &TestWriter,
+	}
+	expectedMessage := "Hello"
+	err = api2.PrintMessage(expectedMessage)
+	if err != nil {
+		t.Errorf("Error trying to use the API2 implementation: %s\n", err.Error())
+	}
+	if TestWriter.Msg != expectedMessage {
+		t.Fatalf("API2 did not write correctly on the io.Writer. \n Actual: %s\nExpected: %s\n", TestWriter.Msg, expectedMessage)
+	}
+}
+
+func TestNormalPrinter_Print(t *testing.T) {
+	expectedMessage := "Hello io.Writer"
+	normal := NormalPrinter{
+		Msg:     expectedMessage,
+		Printer: &PrinterImpl1{},
+	}
+	err := normal.Print()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	testWriter := TestWriter{}
+	normal = NormalPrinter{
+		Msg: expectedMessage,
+		Printer: &PrinterImpl2{
+			Writer: &testWriter,
+		},
+	}
+	err = normal.Print()
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if testWriter.Msg != expectedMessage {
+		t.Errorf("The expected message on the io.Writer doesn't match actual.\n Actual: %s\n Expected: %s\n", testWriter.Msg, expectedMessage)
+	}
+}
+
+func TestPacktPrinter_Print(t *testing.T) {
+	passedMessage := "Hello io.Writer"
+	expectedMessage := "Message from Packt: Hello io.Writer"
+	packt := PacktPrinter{
+		Msg:     passedMessage,
+		Printer: &PrinterImpl1{},
+	}
+	err := packt.Print()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	testWriter := TestWriter{}
+	packt = PacktPrinter{
+		Msg: passedMessage,
+		Printer: &PrinterImpl2{
+			Writer: &testWriter,
+		},
+	}
+	err = packt.Print()
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if testWriter.Msg != expectedMessage {
+		t.Errorf("The expected message on the io.Writer doesn't match actual.\n Actual: %s\n Expected: %s\n", testWriter.Msg, expectedMessage)
+	}
+}
